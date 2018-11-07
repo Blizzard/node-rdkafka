21ecc8651a8ee2806dd2afd7ae412bab5b0c75e5
Alex Alvarez <dominalexican@gmail.com>
2018-11-07 12:48:18 -0500

    AdminClient smoke tests (#519)

---
+++ e2e/admin.spec.js
@@ -0,0 +1,53 @@
+/*
+ * node-rdkafka - Node.js wrapper for RdKafka C/C++ library
+ *
+ * Copyright (c) 2016 Blizzard Entertainment
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE.txt file for details.
+ */
+
+var Kafka = require('../');
+var t = require('assert');
+
+var eventListener = require('./listener');
+var kafkaBrokerList = process.env.KAFKA_HOST || 'localhost:9092';
+
+describe('Admin', function() {
+
+  var client;
+
+  beforeEach(function() {
+      this.timeout(10000);
+      client = Kafka.AdminClient.create({
+        'client.id': 'kafka-test',
+        'metadata.broker.list': kafkaBrokerList
+      });
+  });
+
+
+  it('should create topic sucessfully', function(done) {
+    this.timeout(30000);
+    client.createTopic({
+        topic: 'admin-test-topic',
+        num_partitions: 1,
+        replication_factor: 1
+    }, function(err) {
+      t.ifError(err);
+      done();
+    });
+  });
+
+  it('should raise an error when partitions are larger than number of brokers', function(done) {
+    this.timeout(30000);
+    client.createTopic({
+        topic: 'admin-test-topic',
+        num_partitions: 9999,
+        replication_factor: 9999
+    }, function(err) {
+      t.equal(typeof err, 'object', 'an error should be returned');
+      done();
+    });
+  });
+
+});
