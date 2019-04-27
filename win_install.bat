4538206649a1bc610f8d81cfdbe28f10ecdbfedc
yunnysunny <yunnysunny@gmail.com>
2019-04-27 14:58:55 +0800

    do not install windows-build-tools on windows

--- win_install.bat
+++ win_install.bat
@@ -1,10 +1,10 @@
 @echo off
 choco install openssl.light
-choco uninstall python -y
-if /i %TRAVIS_NODE_VERSION% gtr 6 (
-    npm install --global --production windows-build-tools
-) else (
-    npm install --global --production windows-build-tools@3.1.0
-)
+REM choco uninstall python -y
+REM if /i %TRAVIS_NODE_VERSION% gtr 6 (
+REM     npm install --global --production windows-build-tools
+REM ) else (
+REM     npm install --global --production windows-build-tools@3.1.0
+REM )
 
 choco install make
