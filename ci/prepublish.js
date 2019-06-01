9d0c2d5f4510f44751a2c80d1271b07581f23c81
Stephen Parente <webmakersteve@gmail.com>
2019-06-01 15:22:09 -0700

    Add librdkafka version and presence checks

--- ci/prepublish.js
+++ ci/prepublish.js
@@ -1,17 +1,3 @@
-const path = require('path');
-const fs = require('fs');
-
-const root = path.resolve(__dirname, '..');
-const librdkafkaPath = path.resolve(root, 'deps', 'librdkafka');
-
-// Ensure librdkafka is in the deps directory - this makes sure we don't accidentally
-// publish on a non recursive clone :)
-
-if (!fs.existsSync(librdkafkaPath)) {
-  console.error(`Could not find librdkafka at path ${librdkafkaPath}`);
-  process.exit(1);
-}
-
-// This is a script so it should just run this when it is required
+require('./checks/librdkafka-exists');
+require('./checks/librdkafka-correct-version');
 require('./update-version');
-
