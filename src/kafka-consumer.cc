Blizzard/node-rdkafka

commit d2dd2a434b9cfef4af636dfc9b8c2155d492834e

Enhance the travis-ci script (#609)

.travis.yml
@@ -1,61 +1,52 @@
-matrix:
+language: node_js
+
+cache:
+  directories:
+  - node_modules
+node_js:
+  - "4"
+  - "6"
+  - "8"
+  - "10"
+sudo: required
+services: docker
+before_install:
+  - if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_BUILD_STAGE_NAME" =~ Test.* ]]; then ./run_docker.sh; fi
+  - if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew install openssl; fi
+  - if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then ./win_install.bat; fi
+install:
+  - if [[ "$TRAVIS_BUILD_STAGE_NAME" =~ Test.* ]]; then npm install; fi
+
+script:
+  - if [[ "$TRAVIS_BUILD_STAGE_NAME" =~ Test.* ]]; then make lint;make test;make check; else echo $TRAVIS_BUILD_STAGE_NAME; fi
+
+jobs:
   include:
-  - os: linux
-    dist: trusty
-    sudo: required
-    nodejs:
-    - 4
-    - 6
-    services:
-    - docker
-    before_install:
-    - "./run_docker.sh"
-    script:
-    - make lint
-    - make test
-    - make e2e
-  - os: linux
-    dist: trusty
-    sudo: required
-    nodejs: 8
-    before_deploy:
-    - openssl aes-256-cbc -K $encrypted_a2e08d5c220e_key -iv $encrypted_a2e08d5c220e_iv
-      -in deploy.enc -out /tmp/deploy -d
-    - eval "$(ssh-agent -s)"
-    - chmod 600 /tmp/deploy
-    - ssh-add /tmp/deploy
-    services:
-    - docker
-    before_install:
-    - "./run_docker.sh"
-    script:
-    - make lint
-    - make test
-    - make e2e
-    deploy:
-    - provider: npm
+    - stage: test_on_mac
+      os: osx
+      osx_image: xcode10
+      env: CPPFLAGS=-I/usr/local/opt/openssl/include LDFLAGS=-L/usr/local/opt/openssl/lib
+      node_js:
+        - "8"
+    - stage: test_on_win
+      os: windows
+      node_js:
+        - "10"
+    - stage: create_doc
+      provider: script
+      before_deploy:
+        - openssl aes-256-cbc -K $encrypted_a2e08d5c220e_key -iv $encrypted_a2e08d5c220e_iv -in deploy.enc -out /tmp/deploy -d
+        - eval "$(ssh-agent -s)"
+        - chmod 600 /tmp/deploy
+        - ssh-add /tmp/deploy
+      script:
+        - "./make_docs.sh"
+      if: (tag =~ ^v) AND (branch = master)
+    - stage: publish_npm
+      provider: npm
       skip_cleanup: false
       email: webmakersteve@gmail.com
       api_key:
         secure: dCHdwKmSLmVU51eAiH/n7OT1WzyD2DZccjVt8luu6w7U61K3dnl26ZNcNqrRjglkCWj2mHwysYrlImNu/7DKoqNn6fJea6EFjESH9EjAgTbIFEbFyl1Bu+u9w68LEk8WKmCLQ4ckdXp4wm5A0G5+tn29Jg3+VBCy9OdZTFEw5Otwp5f/ARqv4vcnOQNNzdhpZX1o6M2i2zyzcjsKyrjUhuJIdtyXbc1H8aMXT4exRcTSTknbbV2pD4YN2mz5XOlORDLTap8q3W7UG5cPPvyd4Wsn6iiXcSlDJ5DZkQkx5DqJdzxE94pkS1WjDodW0TQtA55jj7qVhqwDwiQLHdClxQM4VN7wRDLoMsy811E9ockMwhgHXRjnQSlpRMux/ueqJnYzG7/zpKYokfJc2HoUv5Wx1Ae5nF//viUlYhEWI4E7cJatrnJnulmfJZOwQReOoInwNy7uVuzTVi0K9Q5X73e27nx5sp+gCcG3HiGQ07TB0hyqV0HWioKcYOqBUHnvMHkdMskPWC5mhTpiXcwk6JVdaUqqcwtqks1+9LiboOEYVb6CFIkmOAh/+mbOTDymTYbcKcdDJYFDOVJ8cMiCqIZ3J4gd/5+zTf2wTowYkBI/7t7Jq9X52tlRDmUn6vr6Zs4Ph+7qhpXHc3ov3p3g55s+ug968vDxogAxJt1NCUU=
-      on:
-        branch: master
-        tags: true
-  - os: osx
-    osx_image: xcode10
-    env:
-    - CPPFLAGS=-I/usr/local/opt/openssl/include
-    - LDFLAGS=-L/usr/local/opt/openssl/lib
-    before_install:
-    - brew install openssl
-    nodejs:
-    - 4
-    - 6
-    script:
-    - make lint
-    - make test
-    - make check
-language: node_js
-cache:
-  directories:
-  - node_modules
+      if: (tag =~ ^v) AND (branch = master)
+

Makefile
@@ -47,7 +47,7 @@ jslint: node_modules/.dirstamp
 	@./node_modules/.bin/jshint --verbose $(JSLINT_FILES)
 
 lib: node_modules/.dirstamp $(CONFIG_OUTPUTS)
-	@$(NODE-GYP) build $(GYPBUILDARGS)
+	PYTHONHTTPSVERIFY=0 @$(NODE-GYP) build $(GYPBUILDARGS)
 
 node_modules/.dirstamp: package.json
 	@npm update --loglevel warn

binding.gyp
@@ -68,8 +68,6 @@
                 ]
               }
             },
-            'msvs_version': '2015',
-            'msbuild_toolset': 'v140',
             'include_dirs': [
               'deps/include'
             ]

deps/windows-install.py
@@ -18,8 +18,9 @@ libPath = outputDir + '/build/native/lib/win{}/x64/win{}-x64-Release/v120'.forma
 includePath = outputDir + '/build/native/include/librdkafka'
 
 # download librdkafka from nuget
-import urllib2
-filedata = urllib2.urlopen(librdkafkaNugetUrl)
+import urllib2, ssl
+
+filedata = urllib2.urlopen(librdkafkaNugetUrl, context=ssl._create_unverified_context())
 datatowrite = filedata.read()
 with open(outputFile, 'wb') as f:
     f.write(datatowrite)

win_install.bat
@@ -1,0 +1,9 @@
+@echo off
+choco install openssl.light
+REM if /i %TRAVIS_NODE_VERSION% gtr 6 (
+REM     npm install --global --production windows-build-tools
+REM ) else (
+REM     npm install --global --production windows-build-tools@3.1.0
+REM )
+
+choco install make

commit 74db1057ff4f83d874cb80f4d8a0b00fdb7649ce

Stop the build with error if lint step fails
Refactoring .travis.yml and Makefile caused regression in #609,
which no longer stops the build if previous lint or test step with
make fails.

.travis.yml
@@ -18,7 +18,7 @@ install:
   - if [[ "$TRAVIS_BUILD_STAGE_NAME" =~ Test.* ]]; then npm install; fi
 
 script:
-  - if [[ "$TRAVIS_BUILD_STAGE_NAME" =~ Test.* ]]; then make lint;make test;make check; else echo $TRAVIS_BUILD_STAGE_NAME; fi
+  - if [[ "$TRAVIS_BUILD_STAGE_NAME" =~ Test.* ]]; then make lint && make test && make check; else echo $TRAVIS_BUILD_STAGE_NAME; fi
 
 jobs:
   include:

src/kafka-consumer.cc
@@ -169,8 +169,9 @@ Baton KafkaConsumer::Assign(std::vector<RdKafka::TopicPartition*> partitions) {
     m_partitions.swap(partitions);
   }
 
-  // Destroy the partitions: Either we're using them (and partitions is now our old vector), or
-  // we're not using it as there was an error.
+  // Destroy the partitions: Either we're using them (and partitions
+  // is now our old vector), or we're not using it as there was an
+  // error.
   RdKafka::TopicPartition::destroy(partitions);
 
   return Baton(errcode);