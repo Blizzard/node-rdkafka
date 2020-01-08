7f1ffbfaffdc4049c8324553391eb1c7ea4dbc8c
Matt Dowds <matthewd99@gmail.com>
2020-01-08 17:54:21 +0000

    Add support for python 2 and 3 when installing on Windows

--- deps/windows-install.py
+++ deps/windows-install.py
@@ -18,9 +18,16 @@ libPath = outputDir + '/build/native/lib/win{}/x64/win{}-x64-Release/v120'.forma
 includePath = outputDir + '/build/native/include/librdkafka'
 
 # download librdkafka from nuget
-import urllib2, ssl
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+import ssl
 
-filedata = urllib2.urlopen(librdkafkaNugetUrl, context=ssl._create_unverified_context())
+filedata = urlopen(librdkafkaNugetUrl, context=ssl._create_unverified_context())
+
 datatowrite = filedata.read()
 with open(outputFile, 'wb') as f:
     f.write(datatowrite)
