82ddbb0e71f43764e0840dc80d6fd85c23a42973
Gary Wilber <41303831+GaryWilber@users.noreply.github.com>
2020-12-07 12:50:09 -0800

    Keep the print line

--- deps/windows-install.py
+++ deps/windows-install.py
@@ -14,6 +14,7 @@ buildReleaseDir = 'Release'
 env_dist = os.environ
 downloadBaseUrl = env_dist['NODE_RDKAFKA_NUGET_BASE_URL'] if 'NODE_RDKAFKA_NUGET_BASE_URL' in env_dist else 'https://globalcdn.nuget.org/packages/'
 librdkafkaNugetUrl = downloadBaseUrl + 'librdkafka.redist.{}.nupkg'.format(librdkafkaVersion)
+print('download librdkafka form ' + librdkafkaNugetUrl)
 outputDir = 'librdkafka.redist'
 outputFile = outputDir + '.zip'
 dllPath = outputDir + '/runtimes/win{}-x64/native'.format(librdkafkaWinSufix)
