218dd116faff9d086d8e00441e51bcbe29ae84b8
Michael Matloka <dev@twixes.com>
2020-12-09 16:57:50 +0100

    Add test for Worker threads

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,3 +6,19 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
  */
+
+const worker = require('worker_threads');
+const t = require('assert');
+
+module.exports = {
+  'Node package': {
+    'should not throw an error when module is loaded inside a worker': function() {
+      t.doesNotThrow(function() {
+        const code = `
+          require('../build/Release/node-librdkafka')
+        `
+        new worker.Worker(code, { eval: true })
+      })
+    },
+  }
+}
