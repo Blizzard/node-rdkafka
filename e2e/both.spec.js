51c104dd4840ace1f90f8bbc33ec395aae25f4f9
Branislav Katreniak <bkatreni@cisco.com>
2023-11-06 13:31:05 +0100

    e2e/both: fix default consume timeout in reaching eof test
    One round poll to broker is needed to read the message.
    Second round poll to broker is needed to detect EOF.
    
    The test is asserting that we get message and EOF within 2s.
    But the consumer is configured with 'fetch.wait.max.ms': 1000.
    We don't fit 2 timout out polls within 2s.
    
    We need to increase the consume timeout to 3s.

--- e2e/both.spec.js
+++ e2e/both.spec.js
@@ -229,7 +229,7 @@ describe('Consumer/Producer', function() {
       setTimeout(function() {
         producer.produce(topic, null, buffer, null);
       }, 500)
-      consumer.setDefaultConsumeTimeout(2000);
+      consumer.setDefaultConsumeTimeout(3000);
       consumer.consume(1000, function(err, messages) {
         t.ifError(err);
         t.equal(messages.length, 1);
