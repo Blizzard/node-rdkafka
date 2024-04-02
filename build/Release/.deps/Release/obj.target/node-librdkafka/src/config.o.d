a6700165a98175b2cd04064f587840cca202f01f
Wiktor Obrębski <psychowico@gmail.com>
2024-04-02 19:25:57 +0200

    Add static, builded version of librdkafka bindings

---
+++ build/Release/.deps/Release/obj.target/node-librdkafka/src/config.o.d
@@ -0,0 +1,170 @@
+cmd_Release/obj.target/node-librdkafka/src/config.o := g++ -o Release/obj.target/node-librdkafka/src/config.o ../src/config.cc '-DNODE_GYP_MODULE_NAME=node-librdkafka' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/node/.cache/node-gyp/20.10.0/include/node -I/home/node/.cache/node-gyp/20.10.0/src -I/home/node/.cache/node-gyp/20.10.0/deps/openssl/config -I/home/node/.cache/node-gyp/20.10.0/deps/openssl/openssl/include -I/home/node/.cache/node-gyp/20.10.0/deps/uv/include -I/home/node/.cache/node-gyp/20.10.0/deps/zlib -I/home/node/.cache/node-gyp/20.10.0/deps/v8/include -I../node_modules/nan -I/home/node/api -I../deps/librdkafka/src -I../deps/librdkafka/src-cpp  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-exceptions -std=gnu++17 -std=c++17 -MMD -MF ./Release/.deps/Release/obj.target/node-librdkafka/src/config.o.d.raw   -c
+Release/obj.target/node-librdkafka/src/config.o: ../src/config.cc \
+ /home/node/api/src/config.h ../node_modules/nan/nan.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node_version.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/uv.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/uv/errno.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/uv/version.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/uv/unix.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/uv/threadpool.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/uv/linux.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/cppgc/common.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8config.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-array-buffer.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-local-handle.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-internal.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-version.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8config.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-object.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-maybe.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-persistent-handle.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-weak-callback-info.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-primitive.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-data.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-value.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-traced-handle.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-container.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-context.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-snapshot.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-date.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-debug.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-script.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-callbacks.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-promise.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-message.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-exception.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-extension.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-external.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-function.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-function-callback.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-template.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-memory-span.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-initialization.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-isolate.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-embedder-heap.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-microtask.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-statistics.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-unwinder.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-embedder-state-scope.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-platform.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-json.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-locker.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-microtask-queue.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-primitive-object.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-proxy.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-regexp.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-typed-array.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-value-serializer.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/v8-wasm.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node_version.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node_api.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/js_native_api.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/js_native_api_types.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node_api_types.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node_buffer.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node.h \
+ /home/node/.cache/node-gyp/20.10.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h ../node_modules/nan/nan_scriptorigin.h \
+ ../deps/librdkafka/src-cpp/rdkafkacpp.h /home/node/api/src/common.h \
+ ../deps/librdkafka/src/rdkafka.h /home/node/api/src/errors.h \
+ /home/node/api/src/callbacks.h
+../src/config.cc:
+/home/node/api/src/config.h:
+../node_modules/nan/nan.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node_version.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/uv.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/uv/errno.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/uv/version.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/uv/unix.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/uv/threadpool.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/uv/linux.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/cppgc/common.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8config.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-array-buffer.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-local-handle.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-internal.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-version.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8config.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-object.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-maybe.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-persistent-handle.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-weak-callback-info.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-primitive.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-data.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-value.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-traced-handle.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-container.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-context.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-snapshot.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-date.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-debug.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-script.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-callbacks.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-promise.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-message.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-exception.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-extension.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-external.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-function.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-function-callback.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-template.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-memory-span.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-initialization.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-isolate.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-embedder-heap.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-microtask.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-statistics.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-unwinder.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-embedder-state-scope.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-platform.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-json.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-locker.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-microtask-queue.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-primitive-object.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-proxy.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-regexp.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-typed-array.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-value-serializer.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/v8-wasm.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node_version.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node_api.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/js_native_api.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/js_native_api_types.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node_api_types.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node_buffer.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node.h:
+/home/node/.cache/node-gyp/20.10.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+../node_modules/nan/nan_scriptorigin.h:
+../deps/librdkafka/src-cpp/rdkafkacpp.h:
+/home/node/api/src/common.h:
+../deps/librdkafka/src/rdkafka.h:
+/home/node/api/src/errors.h:
+/home/node/api/src/callbacks.h:
