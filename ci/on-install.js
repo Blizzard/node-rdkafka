e133ee9752f48829afce6b6239b21c5349de31a2
Wiktor Obrębski <psychowico@gmail.com>
2024-04-26 14:48:12 +0200

    Migrate prebuilds to use `tar.gz` for download speed

--- ci/on-install.js
+++ ci/on-install.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 async function main() {
   if (fs.existsSync('./build')) {
-    console.log('node-rdkafka bindings already installed, skipping');
+    process.stdout.write('-- node-rdkafka bindings already installed, skipping\n');
     return;
   }
 
-  const prebuildDirectory = `./prebuild/platform-${process.arch}/ABI-${process.versions.modules}/build`;
+  const prebuildFileName = `platform-${process.arch}-ABI-${process.versions.modules}`;
+  const prebuildFilePath = `./prebuild/${prebuildFileName}.tar.gz`;
 
-  if (fs.existsSync(prebuildDirectory)) {
-    await fs.promises.rename(prebuildDirectory, './build');
+
+  if (fs.existsSync(prebuildFilePath)) {
+    process.stdout.write(`-- Unpacking "${prebuildFilePath}" archive...\n`);
+    const tarCmd = spawn('tar', [
+      'xzvf',
+      `./prebuild/${prebuildFileName}.tar.gz`
+    ]);
+    tarCmd.stdout.pipe(process.stdout);
+    tarCmd.stderr.pipe(process.stderr)
   } else {
     throw new Error(`Missing node-rdkafka for arch "${process.arch}" and ABI "${process.versions.modules}". Prebuild bindings first.`);
   }
--- ci/on-prebuild.js
+++ ci/on-prebuild.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 async function main() {
-  const prebuildDirectory = `./prebuild/platform-${process.arch}/ABI-${process.versions.modules}/build`;
-  await fs.promises.mkdir(prebuildDirectory, { recursive: true });
+  const prebuildFileName = `platform-${process.arch}-ABI-${process.versions.modules}`;
 
-
-  await fs.promises.rename('./build', prebuildDirectory);
-
+  process.stdout.write(`Preparing "./prebuild/${prebuildFileName}.tar.gz" archive...\n`);
+  const tarCmd = spawn('tar', [
+    'czvf',
+    `./prebuild/${prebuildFileName}.tar.gz`,
+    './build',
+  ]);
+  tarCmd.stdout.pipe(process.stdout);
+  tarCmd.stderr.pipe(process.stderr)
 }
 
 main().catch(err => console.error(err));
