b434c17533ca09a6a3b17c11aa1cb634fabffdbe
Wiktor Obrębski <psychowico@gmail.com>
2024-04-26 10:16:20 +0200

    Fix oninstall scripts

--- ci/on-install.js
+++ ci/on-install.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 
 async function main() {
-  if (await fs.exists('./build')) {
+  if (fs.existsSync('./build')) {
     console.log('node-rdkafka bindings already installed, skipping');
     return;
   }
 
   const prebuildDirectory = `./prebuild/platform-${process.arch}/ABI-${process.versions.modules}/build`;
 
-  if (await fs.exists(prebuildDirectory)) {
+  if (fs.existsSync(prebuildDirectory)) {
     await fs.promises.rename(prebuildDirectory, './build');
   } else {
-    throw new Error(`Missing node-rdkafka for arch "${process.arch}" and ABI "${process.versions.modules}". Prebuild bindings first.`);
+    throw new Error(`Mispsing node-rdkafka for arch "${process.arch}" and ABI "${process.versions.modules}". Prebuild bindings first.`);
   }
 }
 
