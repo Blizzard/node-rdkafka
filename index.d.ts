43e980f6de71a98b547d4a24576641857d88859a
Tapani Moilanen <moilanen.tapani@gmail.com>
2024-06-26 00:35:42 +0300

    Export event types for use with abstract Client class
    Without exported event types, they must be duplicated in dependant code to correctly use the abstract `Client` class.

--- index.d.ts
+++ index.d.ts
@@ -142,9 +142,9 @@ export interface ConsumerStream extends Readable {
     close(cb?: () => void): void;
 }
 
-type KafkaClientEvents = 'disconnected' | 'ready' | 'connection.failure' | 'event.error' | 'event.stats' | 'event.log' | 'event.event' | 'event.throttle';
-type KafkaConsumerEvents = 'data' | 'partition.eof' | 'rebalance' | 'rebalance.error' | 'subscribed' | 'unsubscribed' | 'unsubscribe' | 'offset.commit' | KafkaClientEvents;
-type KafkaProducerEvents = 'delivery-report' | KafkaClientEvents;
+export type KafkaClientEvents = 'disconnected' | 'ready' | 'connection.failure' | 'event.error' | 'event.stats' | 'event.log' | 'event.event' | 'event.throttle';
+export type KafkaConsumerEvents = 'data' | 'partition.eof' | 'rebalance' | 'rebalance.error' | 'subscribed' | 'unsubscribed' | 'unsubscribe' | 'offset.commit' | KafkaClientEvents;
+export type KafkaProducerEvents = 'delivery-report' | KafkaClientEvents;
 
 type EventListenerMap = {
     // ### Client
