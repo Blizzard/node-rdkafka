af037da71a15d34c5bd2fd31f686b37e7f9da4fd
Constantin Dimitrov <constantind@users.noreply.github.com>
2025-08-08 23:08:20 +0200

    Fix: segfault due to use after free in workers thread
    NodeDisconnect calls worker.WorkComplete() tells V8 persistent callback to be freed, while the thread is still alive, creating use after free segmentation fault.
    
    Fixes https://github.com/Blizzard/node-rdkafka/issues/1057

--- src/workers.cc
+++ src/workers.cc
@@ -767,7 +767,10 @@ void KafkaConsumerConsumeLoop::HandleMessageCallback(RdKafka::Message* msg, RdKa
     delete msg;
   }
 
-  callback->Call(argc, argv);
+  // Don't call after work completed
+  if (callback && !callback->IsEmpty()) {
+    callback->Call(argc, argv);
+  }
 }
 
 void KafkaConsumerConsumeLoop::HandleOKCallback() {
